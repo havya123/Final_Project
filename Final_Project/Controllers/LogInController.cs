11f01eb09c248c007b1bfb9c859bdacedd5cac8b
pietyfilial <trannguyenhieutrung@gmail.com>
2022-12-30 22:01:52 +0700

    sign in

---
+++ Final_Project/Controllers/LogInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
